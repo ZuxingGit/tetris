cd2d35d213cff07be770a918ae5f08a3c905722d
Zuxing_a1816653 <a1816653@adelaide.edu.au>
2026-01-23 17:09:21 +1030

    colors

--- lib/values.dart
+++ lib/values.dart
@@ -46,10 +46,10 @@ samples
 
 const Map<Tetromino, Color> tetrominoColors = {
   Tetromino.L: Color(0xFFFFA500), // Orange
-  Tetromino.J: Color.fromARGB(255, 0, 102, 255), // Blue
+  Tetromino.J: Color.fromARGB(255, 34, 113, 231), // Blue
   Tetromino.I: Color.fromARGB(255, 242, 0, 255), // Pink
   Tetromino.O: Color(0xFFFFFF00), // Yellow
-  Tetromino.S: Color(0xFF008000), // Green
+  Tetromino.S: Color.fromARGB(255, 11, 237, 11), // Green
   Tetromino.Z: Color(0xFFFF0000), // Red
-  Tetromino.T: Color.fromARGB(255, 144, 0, 255), // Purple
+  Tetromino.T: Color.fromARGB(255, 147, 19, 245), // Purple
 };
